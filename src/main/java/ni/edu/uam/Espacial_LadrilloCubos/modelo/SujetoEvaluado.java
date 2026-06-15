baa33510ac69253786ff2b1d72e9f6c4d0c01a30
raul-valverde <raulvruiz@uamv.edu.ni>
2026-06-15 17:02:59 -0600

    feat: Agrego atributos a la clase SSujetoEvaluado

--- src/main/java/ni/edu/uam/Espacial_LadrilloCubos/modelo/SujetoEvaluado.java
+++ src/main/java/ni/edu/uam/Espacial_LadrilloCubos/modelo/SujetoEvaluado.java
@@ -1,4 +1,30 @@
 package ni.edu.uam.Espacial_LadrilloCubos.modelo;
 
-public class SujetoEvaluado {
+
+    import java.util.*;
+import javax.persistence.*;
+import org.openxava.annotations.*;
+
+    @Entity
+    @View(members="idUsuario, nombreCompleto; correoElectronico, contrasenia; edad")
+    public class SujetoEvaluado {
+
+        @Id
+        @Column(length=32)
+        @Required
+        private String idUsuario;
+
+        @Column(length=100)
+        @Required
+        private String nombreCompleto;
+
+        @Column(length=100)
+        @Required
+        private String correoElectronico;
+
+        @Column(length=50)
+        private String contrasenia;
+
+        @Required
+        private int edad;
 }
