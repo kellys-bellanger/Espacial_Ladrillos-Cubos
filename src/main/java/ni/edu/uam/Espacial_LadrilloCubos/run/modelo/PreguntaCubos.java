5941022ed006af08626f35dc64d223e678ea592b
TheMarconic12 <masalas@uamv.edu.ni>
2026-06-15 11:20:33 -0600

    guardando cambios pendientes

--- src/main/java/ni/edu/uam/Espacial_LadrilloCubos/run/modelo/PreguntaCubos.java
+++ src/main/java/ni/edu/uam/Espacial_LadrilloCubos/run/modelo/PreguntaCubos.java
@@ -1,4 +1,40 @@
 package ni.edu.uam.Espacial_LadrilloCubos.run.modelo;
 
+import javax.persistence.*;
+import org.openxava.annotations.*;
+
+@Entity
+@Tab(properties = "enunciado, opcionA, opcionB, opcionC, opcionD, respuestaCorrecta")
 public class PreguntaCubos {
+
+    @Id @GeneratedValue
+    @Hidden
+    private Long id;
+
+    @Required
+    @Column(length = 500)
+    @RowStyle(style = "", property = "", value = "")
+    private String enunciado;
+
+    @Required
+    private String opcionA;
+
+    @Required
+    private String opcionB;
+
+    @Required
+    private String opcionC;
+
+    @Required
+    private String opcionD;
+
+    @Required
+    private String respuestaCorrecta; // A, B, C o D
+
+    @Required
+    @Stereotype("IMAGE")
+    @Column(length = 250)
+    private String rutaImagenCubo;
+
+
 }
