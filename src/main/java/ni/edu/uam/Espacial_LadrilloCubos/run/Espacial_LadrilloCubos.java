5a823d91d4ceacf1792ba0b75a8656b5b28c3eee
raul-valverde <raulvruiz@uamv.edu.ni>
2026-06-22 18:15:21 -0600

    Fix: Eliminado frontend conflictivo de pruebas y configurado initDB en Main

--- src/main/java/ni/edu/uam/Espacial_LadrilloCubos/run/Espacial_LadrilloCubos.java
+++ src/main/java/ni/edu/uam/Espacial_LadrilloCubos/run/Espacial_LadrilloCubos.java
@@ -9,6 +9,7 @@ import org.openxava.util.*;
 public class Espacial_LadrilloCubos {
 
 	public static void main(String[] args) throws Exception {
+		System.setProperty("openxava.initDB", "true");
 		DBServer.start("Espacial_LadrilloCubos-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
 		AppServer.run("Espacial_LadrilloCubos"); // Usa AppServer.run("") para funcionar en el contexto raíz
 	}
